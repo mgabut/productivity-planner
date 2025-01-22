3bbe5a88c83dcceee326612d7004ad47dd9853ea
mgabut <mgabut.pro@gmail.com>
2025-01-22 12:38:41 +0100

    fix chore (env + firebase)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +8,4 @@ import { environment } from '../environments/environment';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly isStaging = !(environment as any).production;
 }
