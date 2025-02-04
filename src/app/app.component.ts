6beb4cbad95b7fa45eaf61dbc88abec4494ffb99
mgabut <mgabut.pro@gmail.com>
2025-02-04 17:08:42 +0100

    Authentication / Login / Write on Database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthenticationService } from './core/authentication.service';
+import { switchMap } from 'rxjs';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'productivity-planner';
 
@@ -16,7 +18,11 @@ export class AppComponent {
     const email = 'john.doe@gmail.com'
     const password = 'qwertz'
 
-    this.#authenticationService.login(email, password)
-    .subscribe((response) => {console.log(response)})
+    this.#authenticationService.login(email, password).pipe(
+      switchMap(response => {
+        const {email, localId, idToken} = response;
+        return this.#authenticationService.save(email, localId, idToken)
+      })
+    ).subscribe((response)=> console.log(response))
   }
 }
--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,42 +1,64 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
-interface FirebaseResponseRegister{
+interface FirebaseResponseSignUp{
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
 }
-
+interface FirebaseResponseSignIn{
+  displayName: string;
+  email: string;
+  expiresIn: string;
+  idToken: string;
+  localId: string;
+  refreshToken: string;
+  registered: boolean
+}
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  readonly #http = inject(HttpClient);
+  readonly #http = inject(HttpClient)
   
-  register(
-    email: string, 
-    password: string
-  ): Observable<FirebaseResponseRegister> {
+  register(email: string, password: string): Observable<FirebaseResponseSignUp> {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseConfig.apiKey}`;
     const body = { email, password, returnSecureToken: true };
     
-    return this.#http.post<FirebaseResponseRegister>(url, body)
+    return this.#http.post<FirebaseResponseSignUp>(url, body);
   }
 
-  login(email: string, password: string): Observable<unknown> {
+  login(email: string, password: string): Observable<FirebaseResponseSignIn> {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseConfig.apiKey}`;
     const body = { email, password, returnSecureToken: true };
   
+    return this.#http.post<FirebaseResponseSignIn>(url, body) 
+  }
 
-    return this.#http.post<unknown>(url, body)
+  save (email: string, userId: string, bearerToken: string): Observable<unknown>{
+    const baseUrl = `https://firestore.googleapis.com/v1/projects/${environment.firebaseConfig.projectId}/databases/(default)/documents`
     
+    const userFirestoreCollectionId = 'users'
+    const Url = `${baseUrl}/${userFirestoreCollectionId}?key=${environment.firebaseConfig.apiKey}&documentId=${userId}`
+
+    const body = {
+      fields: {
+        email: {stringValue: email}
+      }
+    }
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${bearerToken}`
+    })
+    const options = { headers} ;
+
+    return this.#http.post<unknown>(Url, body, options)
   }
 }
-
 /*
 https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[API_KEY]
 AIzaSyAZE7hGzoxsIITViYGzZZy5v90ocRvXhbo
