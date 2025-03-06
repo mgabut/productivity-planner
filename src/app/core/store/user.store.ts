56fa078e20be22286f53f78fce69ce162609d261
mgabut <mgabut.pro@gmail.com>
2025-03-06 15:34:55 +0100

    Create user.store.ts

---
+++ src/app/core/store/user.store.ts
@@ -0,0 +1,34 @@
+import { computed, inject } from "@angular/core";
+import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals";
+import { AuthenticationService } from "../authentication.service";
+
+interface UserState {
+    username: string,
+    email: string,
+  };
+
+
+export const UserStore = signalStore(
+    { providedIn : 'root'},
+    withState<UserState>({
+        username: '',
+        email: '',
+    }),
+
+    withComputed((store) => {
+
+      const isGoogleUser = computed(() => 
+        store.email().endsWith('@google.com'))
+      
+      return {isGoogleUser}
+    }),
+    
+    withMethods((store, authenticationService = inject(AuthenticationService)) => ({
+      register(email: string, password: string):void {
+        authenticationService.register(email, password).subscribe(
+          (response) => {
+            patchState(store, {email: response.userId})
+          } 
+      )}
+    }))
+);
